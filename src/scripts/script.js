8b7b7c1244b61a424d9151383f88c57b11b79673
Alexander Rusak <375298388602@mail.ru>
2021-03-07 18:43:36 +0300

    added push to Complete section with LS

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -2,15 +2,12 @@ const getElementById = (id) => {
     return document.getElementById(id);
 }
 
-const btn_newTask = getElementById("new-task");
-btn_newTask.addEventListener("click", () => {
-    console.log("ok");
+getElementById("new-task").addEventListener("click", () => {
     addTask();
+});
 
-})
 
 
-let map_removeButton;
 
 const getMapTodoFromLocalStarage = () => {
     const todos = [];
@@ -22,7 +19,7 @@ const getMapTodoFromLocalStarage = () => {
     }
     return todos;
 }
-const getCurrentTasksLITemplate = (id, title, text) => {
+const getTasksLITemplate = (title, text, status) => {
     return (`
     <div class="w-100 mr-2">
         <div class="d-flex w-100 justify-content-between">
@@ -30,6 +27,10 @@ const getCurrentTasksLITemplate = (id, title, text) => {
         </div>
         <p class="mb-1 w-100">${text}</p>
     </div>
+        ${!status ? getCurrentAdditionalTemplate():""}`);
+}
+const getCurrentAdditionalTemplate = () => {
+    return (`
     <div class="dropdown m-2 dropleft">
         <button class="btn btn-secondary h-100" type="button" id="dropdownMenuItem1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 <i class="fas fa-ellipsis-v" aria-hidden="true"></i>
@@ -39,7 +40,16 @@ const getCurrentTasksLITemplate = (id, title, text) => {
           <button type="button" class="btn btn-info w-100 my-2">Edit</button>
           <button type="button" class="btn btn-danger w-100">Delete</button>
         </div>
-    </div>`);
+    </div>`
+    );
+}
+const removeToDoItem = (selectedNode) => {
+    const key = selectedNode.parentNode.parentNode.parentNode.id;
+    console.log(key);
+    const deletingElement = getElementById(key);
+    deletingElement.parentNode.removeChild(deletingElement);
+    localStorage.removeItem(+key);
+
 }
 
 const editTodoTask = (selectedNode) => {
@@ -48,14 +58,26 @@ const editTodoTask = (selectedNode) => {
     getElementById("exampleModalLabel").innerText = "Edit task";
     const btn_editTask = getElementById("form-group-save-task");
     btn_editTask.innerText = "Edit task";
-    const node_id = selectedNode.parentNode.parentNode.parentNode.id;
-    const { id, title, text, radio: priority } = JSON.parse(localStorage.getItem(node_id));
+    const node_id = +selectedNode.parentNode.parentNode.parentNode.id;
+    /* const { id, title, text, radio: priority, isCompleted } = localStorage.getItem(node_id); */
+    const { id, title, text, radio: priority, isCompleted } = JSON.parse(localStorage.getItem(node_id));
+    console.log(id, title, text, priority, isCompleted);
     setModalValues(title, text, priority);
     btn_editTask.addEventListener("click", () => {
         localStorage.removeItem(id);
-        addTodoToLocalStorage(id, id, ...getModalValues());
+        addTodoToLocalStorage(id, id, ...getModalValues(), isCompleted);
 
     })
+}
+const completeTodoTask = (selectedNode) => {
+    const node_id = +selectedNode.parentNode.parentNode.parentNode.id;
+    console.log(node_id);
+    console.log(localStorage.getItem(node_id));
+    const { id, title, text, radio: priority, isCompleted } = JSON.parse(localStorage.getItem(node_id));
+    localStorage.removeItem(+node_id);
+    addTodoToLocalStorage(node_id, id, title, text, priority, !isCompleted);
+    location.reload();
+
 }
 
 const getModalValues = () => {
@@ -67,66 +89,106 @@ const getModalValues = () => {
 const setModalValues = (title, text, priority) => {
     getElementById("inputTitle").value = title;
     getElementById("inputText").value = text;
-    getElementById(priority).setAttribute("checked", "checked");
+    getElementById(`${priority}`).setAttribute("checked", "checked");
 }
 
 const setClicksOnNode = (node, func) => {
     for (let i = 0; i < node.length; i++) {
         node[i].addEventListener("click", () => {
+            console.log("ok");
             func(node[i]);
         })
     }
 }
 
-const removeToDoItem = (selectedNode) => {
-    const id = selectedNode.parentNode.parentNode.parentNode.id;
-    const deletingElement = getElementById(`${id}`);
-    deletingElement.parentNode.removeChild(deletingElement)
-    localStorage.removeItem(id);
-}
-const getNodeTodoListItem = () => {
-    const todos = getMapTodoFromLocalStarage();
-    console.log(todos);
-    const getNodeULTodoItem = getElementById("currentTasks");
-    todos.map(({ id, title, text }) => {
-        const todoLINode = document.createElement("li");
-        todoLINode.setAttribute("class", "list-group-item d-flex w-100 mb-2");
-        todoLINode.setAttribute("id", `${id}`);
-        getNodeULTodoItem.append(todoLINode);
-        todoLINode.innerHTML = getCurrentTasksLITemplate(id, title, text);
 
-    })
-    map_removeButton = document.querySelectorAll(".btn-danger");
-    setClicksOnNode(map_removeButton, removeToDoItem);
 
-    const map_editButton = document.querySelectorAll(".btn-info");
-    setClicksOnNode(map_editButton, editTodoTask);
-}
-const addTodoToLocalStorage = (key, id, title, text, radio) => {
+const addTodoToLocalStorage = (key, id, title, text, radio, isCompleted) => {
     const todoObject = {
         id,
         title,
         text,
-        radio
+        radio,
+        isCompleted
     }
     localStorage.setItem(key, JSON.stringify(todoObject));
 }
 
+const appendTodoElements = (completedStatus) => {
+    const currentTodos = getMapTodoFromLocalStarage().filter(todo => todo.isCompleted == completedStatus);
+    console.log(currentTodos);
+    const currentNode = completedStatus ? getElementById("completedTasks") : getElementById("currentTasks");
+
+    currentTodos.map(({ id, title, text }) => {
+        const todoLINode = document.createElement("li");
+        todoLINode.setAttribute("class", "list-group-item d-flex w-100 mb-2");
+        todoLINode.setAttribute("id", `${id} `);
+        currentNode.append(todoLINode);
+        todoLINode.innerHTML = getTasksLITemplate(title, text, completedStatus);
+
+    })
+    if (!completedStatus) {
+        const map_removeButton = document.querySelectorAll(".btn-danger");
+        setClicksOnNode(map_removeButton, removeToDoItem);
+
+        const map_editButton = document.querySelectorAll(".btn-info");
+        setClicksOnNode(map_editButton, editTodoTask);
+
+        const map_completeButton = document.querySelectorAll(".btn-success");
+        setClicksOnNode(map_completeButton, completeTodoTask);
+
+    }
+
+}
 
 const addTask = () => {
     const btn_saveTask = getElementById("form-group-save-task");
     $('#exampleModal').modal("show");
     btn_saveTask.addEventListener("click", () => {
-        console.log("ok");
         const id_dateValue = +new Date;
-        addTodoToLocalStorage(id_dateValue, id_dateValue, ...getModalValues());
+        addTodoToLocalStorage(id_dateValue, id_dateValue, ...getModalValues(), false);
     });
 }
 
 
 
 
-getNodeTodoListItem();
 
+appendTodoElements(true);
+appendTodoElements(false);
 
+/* const showListTodos = () => {
+    const currentTodos = getMapTodoFromLocalStarage();
+    console.log(currentTodos);
+    const completed = currentTodos.filter(todo => todo.isCompleted == true);
+    const current = currentTodos.filter(todo => todo.isCompleted == false);
+    const currentTasksNode = getElementById("currentTasks");
+    const completedTasksNode = getElementById("completedTasks");
 
+    current.map(({ id, title, text }) => {
+        const todoCurrentLINode = document.createElement("li");
+        todoCurrentLINode.setAttribute("class", "list-group-item d-flex w-100 mb-2");
+        todoCurrentLINode.setAttribute("id", `${ id } `);
+        currentTasksNode.append(todoCurrentLINode);
+        todoCurrentLINode.innerHTML = getCurrentTasksLITemplate(title, text);
+
+    })
+    completed.map(({ id, title, text }) => {
+        const todoCompleteLINode = document.createElement("li");
+        todoCompleteLINode.setAttribute("class", "list-group-item d-flex w-100 mb-2");
+        todoCompleteLINode.setAttribute("id", `${ id } `);
+        completedTasksNode.append(todoCompleteLINode);
+        todoCompleteLINode.innerHTML = getCompleteTasksLITemplate(title, text);
+
+    })
+    const map_removeButton = document.querySelectorAll(".btn-danger");
+    setClicksOnNode(map_removeButton, removeToDoItem);
+
+    const map_editButton = document.querySelectorAll(".btn-info");
+    setClicksOnNode(map_editButton, editTodoTask);
+
+    const map_completeButton = document.querySelectorAll(".btn-success");
+    setClicksOnNode(map_completeButton, completeTodoTask);
+} */
+
+//showListTodos();
